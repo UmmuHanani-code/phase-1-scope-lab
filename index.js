e23cf901a2d699edc0f314ac120a1b4e4e19a41c
Nuria Mohamed Hussein <nuria.hussein@student.moringaschool.com>
2024-12-11 15:15:16 +0300

    commit all

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Write your solution in this file!
+
+var customerName = "bob";
+
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+    return customerName;
+}
+
+upperCaseCustomerName();
+
+var  bestCustomer;
+
+function setBestCustomer() {
+
+    bestCustomer = 'not bob';
+   return bestCustomer;
+
+  }
+
+   setBestCustomer();
+
+function overwriteBestCustomer(newBestCustomer) {
+  bestCustomer = newBestCustomer;
+
+}
+overwriteBestCustomer('maybe bob');
+
+const leastFavoriteCustomer = 'mike';
+
+function changeLeastFavoriteCustomer() {
+    return leastFavoriteCustomer = 'jane';
+}
+
+console.log(changeLeastFavoriteCustomer());
